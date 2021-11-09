9774248162968fef939a4990bc7e28da70762c70
Your Name <hydrurdgn@gmail.com>
2021-11-09 10:33:24 +0300

    Seconde Commit

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
+
+//TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
 
 //MİDDLEWARES
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+app.get('/add', function (req, res) {
+  res.render('add');
 });
 
 const PORT = 3000;
